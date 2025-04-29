3bf9685e6cfa44a62e370e1b51e2090f568baed6
haru16-no14 <haru16.nhs@gmail.com>
2025-04-30 01:49:28 +0900

    ホームのリストに"現在地を表示"を追加

--- WeatherApp/View/HomeView.swift
+++ WeatherApp/View/HomeView.swift
@@ -1,22 +1,37 @@
-//
-//  HomeView.swift
-//  WeatherApp
-
+import CoreLocation
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject private var locationManager = LocationManager()
+
     var regions: [Region] {
-        Region.allCases
+        Region.allRegions
     }
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(regions, id: \.self) { region in
-                    NavigationLink {
-                        WeatherView(region: region)
-                    } label: {
-                        Text(region.name)
+            VStack {
+                List {
+                    ForEach(regions, id: \.self) { region in
+                        NavigationLink {
+                            WeatherView(region: region)
+                        } label: {
+                            Text(region.name)
+                        }
+                    }
+
+                    NavigationLink(
+                        destination: locationManager.location != nil
+                            ?
+                            AnyView(WeatherView(region: .current(
+                                location: locationManager
+                                    .location!
+                            )))
+                            : AnyView(EmptyView()),
+                        isActive: .constant(locationManager.location != nil)
+                    ) {
+                        Text("現在地を表示")
+                            .foregroundColor(.blue)
                     }
                 }
             }
